4a085298d6856c6e9ebfa267f41c5d184ae23dc8
elina-ruha <elina.ruha@aalto.fi>
2026-05-15 13:28:43 +0300

    Delete book/fix_static.py
    ei toiminutkaan

--- book/fix_static.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def setup(app):
-    def create_static_dir(app):
-        static_dir = os.path.join(app.outdir, '_static')
-        os.makedirs(static_dir, exist_ok=True)
-    
-    app.connect('builder-inited', create_static_dir)
-    return {'version': '0.1'}
